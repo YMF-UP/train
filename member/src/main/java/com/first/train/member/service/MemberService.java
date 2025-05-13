7d1bafbff6309289595415c7f7ef05074b57e20a
yyc <2571853049@qqcom>
2025-05-13 21:53:26 +0800

    集成Mybaits官方代码生成器，访问数据库成功

--- member/src/main/java/com/first/train/member/service/MemberService.java
+++ member/src/main/java/com/first/train/member/service/MemberService.java
@@ -10,7 +10,7 @@ public class MemberService {
     private MemberMapper memberMapper;
     public int count()
     {
-      return memberMapper.count();
+      return Math.toIntExact(memberMapper.countByExample(null));
     }
 
 }

21a4f1d8a3762e9567e80d38f1e78abf74c9a89c
yyc <2571853049@qqcom>
2025-05-14 16:40:41 +0800

    封装请求参数和返回结果

--- member/src/main/java/com/first/train/member/controller/MemberController.java
+++ member/src/main/java/com/first/train/member/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.first.train.member.controller;
 
+import com.first.train.common.rsp.CommonResp;
+import com.first.train.member.req.MemberRegisterReq;
 import com.first.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +15,21 @@ public class MemberController {
     @Resource
     private MemberService memberService;
     @GetMapping("/count")
-    public int count()
-    {
-        return memberService.count();
+    public CommonResp<Integer> count()
+    {   int count= memberService.count();
+        CommonResp<Integer> commonResp = new CommonResp<>();
+        commonResp.setContent(count);
+        return commonResp ;
     }
+    @PostMapping ("/register")
+    public CommonResp<Long> register(MemberRegisterReq req)
+    {
+        long register=memberService.register(req);
+
+        CommonResp<Long> commonResp = new CommonResp<>();
+        commonResp.setContent(register);
+        return commonResp;
+    }
+
 
 }
--- member/src/main/java/com/first/train/member/service/MemberService.java
+++ member/src/main/java/com/first/train/member/service/MemberService.java
@@ -1,8 +1,14 @@
 package com.first.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.first.train.member.domain.Member;
+import com.first.train.member.domain.MemberExample;
 import com.first.train.member.mapper.MemberMapper;
+import com.first.train.member.req.MemberRegisterReq;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -12,5 +18,27 @@ public class MemberService {
     {
       return Math.toIntExact(memberMapper.countByExample(null));
     }
+
+    public long register(MemberRegisterReq req)
+    {
+        String mobile=req.getMobile();
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+
+        if(CollUtil.isNotEmpty(list))
+        {
+           // return list.get(0).getId();
+            throw new RuntimeException("手机号已注册");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+
+        memberMapper.insert(member);
+        return  member.getId();
+    }
+
 
 }

de23827f4fe0f17d73a40c83d07c11f14105d725
yyc <2571853049@qqcom>
2026-08-31 21:43:48 +0800

    feat(business): 实现车次基础信息管理与车次编码唯一性校验

---
+++ business/src/main/java/com/first/train/business/req/TrainQueryReq.java
@@ -0,0 +1,13 @@
+package com.first.train.business.req;
+
+import com.first.train.common.req.PageReq;
+
+public class TrainQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainQueryReq{" +
+                "} " + super.toString();
+    }
+}
+
